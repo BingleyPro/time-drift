995bfb5443e6ade548949e19c63b1506e994a02c
Bingley <127451064+BingleyPro@users.noreply.github.com>
2025-01-30 13:57:10 +1100

    abcdefg

--- scripts/time_object.gd
+++ scripts/time_object.gd
@@ -3,6 +3,21 @@ extends Node
 @export_group("Time Behaviour")
 @export var propagate_past_changes: bool = false
 
+@export_category("Timeline Settings")
+
+@export var exists_in_past: bool = true
+@export var exists_in_present: bool = true
+@export var exists_in_future: bool = true
+
+@export_group("Past Settings")
+@export var broken_in_past: bool = false
+
+@export_group("Present Settings")
+@export var broken_in_present: bool = false
+
+@export_group("Future Settings")
+@export var broken_in_future: bool = false
+
 @onready var past_node = $Past
 @onready var present_node = $Present
 @onready var future_node = $Future
@@ -11,6 +26,8 @@ func _ready () -> void:
 	# Connect to the timeline_changed signal
 	var time_manager = get_node("/root/TimeManager")
 	time_manager.connect("timeline_changed", Callable(self, "_on_timeline_changed"))
+	
+	# TODO: Check exists_in_[timeline] and disable collision for anything else
 	
 	# Make sure inital state matches current timeline
 	_on_timeline_changed(time_manager.current_timeline)
@@ -25,10 +42,13 @@ func _on_timeline_changed(new_timeline) -> void:
 	# Show only the relevant layer
 	match new_timeline:
 		TimeManager.Timeline.PAST:
-			past_node.visible = true
+			if exists_in_past:
+				past_node.visible = true
 		TimeManager.Timeline.PRESENT:
-			present_node.visible = true
+			if exists_in_present:
+				present_node.visible = true
 		TimeManager.Timeline.FUTURE:
-			future_node.visible = true
+			if exists_in_future:
+				future_node.visible = true
 		_:
 			print("Unknown timeline: ", new_timeline)
