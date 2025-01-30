5306423749ecf29e1a97466c80a42210e0f9d0c8
Bingley <me@bingleypro.com>
2025-01-30 21:02:47 +1100

    hi

--- scripts/time_object.gd
+++ scripts/time_object.gd
@@ -9,7 +9,13 @@ extends Node
 @export var exists_in_present: bool = true
 @export var exists_in_future: bool = true
 
+enum Property { BROKEN, EPIC }
+
 @export_group("Past Settings")
+@export var past_properties: Property
+
+@export_enum("Slow:30", "Average:60", "Very Fast:200") var character_speed: int
+
 @export var broken_in_past: bool = false
 
 @export_group("Present Settings")
@@ -27,7 +33,9 @@ func _ready () -> void:
 	var time_manager = get_node("/root/TimeManager")
 	time_manager.connect("timeline_changed", Callable(self, "_on_timeline_changed"))
 	
-	# TODO: Check exists_in_[timeline] and disable collision for anything else
+	#_disable_collisions(past_node)
+	#_disable_collisions(present_node)
+	#_disable_collisions(future_node)
 	
 	# Make sure inital state matches current timeline
 	_on_timeline_changed(time_manager.current_timeline)
@@ -44,11 +52,14 @@ func _on_timeline_changed(new_timeline) -> void:
 		TimeManager.Timeline.PAST:
 			if exists_in_past:
 				past_node.visible = true
+				#_enable_collisions(past_node)
 		TimeManager.Timeline.PRESENT:
 			if exists_in_present:
 				present_node.visible = true
+				#_enable_collisions(present_node)
 		TimeManager.Timeline.FUTURE:
 			if exists_in_future:
 				future_node.visible = true
+				#_enable_collisions(future_node)
 		_:
 			print("Unknown timeline: ", new_timeline)
